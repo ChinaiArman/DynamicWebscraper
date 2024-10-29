091614f4aeaadd6f68bae513524cddf1f43da8f8
Arman Chinai <113311772+ChinaiArman@users.noreply.github.com>
2024-10-29 10:38:52 -0700

    Add data models

--- server/models/Scrape.py
+++ server/models/Scrape.py
@@ -9,4 +9,25 @@ from db_config import db
 class Scrape(db.Model):
     """
     """
-    pass
+    __tablename__ = 'scrapes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    url = db.Column(db.String(1000), nullable=False)
+    prompt = db.Column(db.String(1000), nullable=False)
+    data = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    user = db.relationship('User', back_populates='scrapes')
+
+    def __repr__(self):
+        """
+        """
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'url': self.url,
+            'prompt': self.prompt,
+            'data': self.data,
+            'created_at': self.created_at
+        }
--- server/models/User.py
+++ server/models/User.py
@@ -9,4 +9,26 @@ from db_config import db
 class User(db.Model):
     """
     """
-    pass
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
+    requests_available = db.Column(db.Integer, default=20)
+    reset_code = db.Column(db.String(6), nullable=True)
+
+    scrapes = db.relationship('Scrape', back_populates='user')
+
+    def __repr__(self):
+        """
+        """
+        return {
+            'id': self.id,
+            'email': self.email,
+            'name': self.name,
+            'is_verified': self.is_verified,
+            'requests_available': self.requests_available,
+            'reset_code': self.reset_code
+        }
