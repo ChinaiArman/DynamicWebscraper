778a6e910bf407a7cfa8822f4f33db27f5694224
Natalie Yu <nyu17@my.bcit.ca>
2024-11-02 15:55:18 -0700

    Add scrape function

--- server/services/Scraper.py
+++ server/services/Scraper.py
@@ -2,14 +2,35 @@
 """
 
 # IMPORTS
-
+import requests
+from bs4 import BeautifulSoup
 
 # SCRAPER CLASS
 class Scraper:
     """
+    A class to scrape a website and return the text content.
     """
     def __init__(self):
         """
         """
         pass
+
+    def scrape(self, url: str) -> str:
+        """
+        Scrape a website of a given URL by removing all HTML tags and returning the text content.
+
+        Args
+        ----
+        url (str): The URL to scrape.
+
+        Returns
+        -------
+        str: The text content of the URL.
+        """
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for data in soup(['style', 'script']):
+            data.decompose()
+        text = ' '.join(soup.stripped_strings)
+        return text
     
